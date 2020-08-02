f3505f8bad777ab42340a0ec1947a171be3caade
Dhanush R <dhanushr369@gmail.com>
2020-08-02 16:46:46 +0530

    commit

--- exe6.py
+++ exe6.py
@@ -1,9 +1,9 @@
 m=10
 n=30
-m = int(input("Enter the prime range: "))
-n = int(input("Enter the prime range: "))
+m = int(input("Enter the prime range m: "))
+n = int(input("Enter the prime range n: "))
 
-for num in range(m,n + 1):
+for num in range(-m,-n + 1):
     if num > 1:
         for i in range(2, num):
             if (num % i) == 0:
